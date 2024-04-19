d9a9712bfaa163d93bbd74439cfa6142da05964a
Juan Gomes <juanmartinsgmess@gmail.com>
2024-04-19 17:50:13 -0400

    Adicionando o PicoCss ao projeto

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-
 </script>
 
 <template>
+  <div class="container">
+    <nav>
+      <ul>
+        <li><strong>Kitchen App</strong></li>
+      </ul>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Categories</a></li>
+      </ul>
+    </nav>
+  </div>
   <RouterView />
 </template>
